88044597e2193c0b0a199cde2dd9bc50bc736ec8
LAFFONT_Emile <emile.laffont@gmail.com>
2022-05-30 09:15:53 +0200

    ajout des murs et du personnage

--- src/app/jeu-list/labyrinthe/labyrinthe.component.ts
+++ src/app/jeu-list/labyrinthe/labyrinthe.component.ts
@@ -13,21 +13,21 @@ export class LabyrintheComponent implements OnInit {
   allLevels!: {
     level: number;
     //"target-moves": number;
-    /*positions: {
+    positions: {
       man: number[];
-      boxes: number[][];
-      target: number[][];
-    };*/
+      /*boxes: number[][];
+      target: number[][];*/
+    };
     cases: number[][];
   }[];
   currentLevel!: {
     level: number;
-    /*"target-moves": number;
+    //"target-moves": number;
     positions: {
       man: number[];
-      boxes: number[][];
-      target: number[][];
-    };*/
+      //boxes: number[][];
+      //target: number[][];
+    };
     cases: number[][];
   };
 
@@ -37,17 +37,19 @@ export class LabyrintheComponent implements OnInit {
   public board!: boolean [][];
   
   private sub: any;
+  public manPosition: number[];
+  public currentMoves: number = 0;
   /*public targetMoves: number;
 
   public boxesPosition: number[][];
   public targetsPosition: number[][];
-  public manPosition: number[];
-  public currentMoves: number = 0;
+  
+  
   public hasWon: boolean = false;
   public hasLost: boolean = false;*/
 
   constructor(private route: ActivatedRoute, private router:Router, private LevelService: LevelService ) { 
-    
+    this.manPosition = [];
   }
 
 
@@ -59,8 +61,8 @@ export class LabyrintheComponent implements OnInit {
         this.currentLevel = this.allLevels[this.id];
         this.rows = this.currentLevel.cases.length;
         this.columns = this.currentLevel.cases[0].length;
-        /*this.manPosition = this.currentLevel.positions.man;
-        this.boxesPosition = this.currentLevel.positions.boxes;
+        this.manPosition = this.currentLevel.positions.man;
+        /*this.boxesPosition = this.currentLevel.positions.boxes;
         this.targetsPosition = this.currentLevel.positions.target;
         this.targetMoves = this.currentLevel["target-moves"];*/
         this.board = [];
@@ -77,5 +79,77 @@ export class LabyrintheComponent implements OnInit {
   
   getvalue(h : number, w : number): number {
     return this.currentLevel.cases[h][w];
+  }
+
+  perso(h : number, w : number): boolean {
+    return this.manPosition[0] == h && this.manPosition[1] == w;
+  }
+
+  moveLeft() {
+    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] % 2 != 1) {
+      this.manPosition[1] = this.manPosition[1] - 1;
+      this.increaseMoves();
+    }
+  }
+
+  moveRight() {
+    if (this.pas_a_droite(this.manPosition[0],this.manPosition[1])) {
+      this.manPosition[1] = this.manPosition[1] + 1;
+      this.increaseMoves();
+    }
+  }
+
+  pas_a_droite(l:number, c:number) : boolean{
+    if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 4){
+      return true;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 8){
+      return false;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 12){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  moveUp() {
+    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 8) {
+      this.manPosition[0] = this.manPosition[0] - 1;
+      this.increaseMoves();
+    }
+  }
+
+  moveDown() {
+    if (this.pas_a_bas(this.manPosition[0],this.manPosition[1])) {
+      this.manPosition[0] = this.manPosition[0] + 1;
+      this.increaseMoves();
+    }
+  }
+
+  pas_a_bas(l:number, c:number) : boolean{
+    if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 2){
+      return true;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 4){
+      return false;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 6){
+      return true;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 8){
+      return false;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 10){
+      return true;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 12){
+      return false;
+    }else if(this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 14){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  increaseMoves(): void {
+    this.currentMoves++;
+  }
+
+  refresh(): void {
+    window.location.reload();
   }
 }
