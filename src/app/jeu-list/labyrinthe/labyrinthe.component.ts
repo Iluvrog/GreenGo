b7dcbc11b4b83a3981efd27cd8d6c2925ed08921
LAFFONT_Emile <emile.laffont@gmail.com>
2022-06-01 07:54:52 +0200

    ajout du systeme de clé et de porte

--- src/app/jeu-list/labyrinthe/labyrinthe.component.ts
+++ src/app/jeu-list/labyrinthe/labyrinthe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LevelService } from "./service/level.service";
 import { HotkeysService, Hotkey } from 'angular2-hotkeys';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-labyrinthe',
@@ -15,8 +16,8 @@ export class LabyrintheComponent implements OnInit {
     //"target-moves": number;
     positions: {
       man: number[];
-      /*boxes: number[][];
-      target: number[][];*/
+      doors: number[][];
+      keys: number[][];
     };
     cases: number[][];
   }[];
@@ -25,8 +26,8 @@ export class LabyrintheComponent implements OnInit {
     //"target-moves": number;
     positions: {
       man: number[];
-      //boxes: number[][];
-      //target: number[][];
+      doors: number[][];
+      keys: number[][];
     };
     cases: number[][];
   };
@@ -39,10 +40,12 @@ export class LabyrintheComponent implements OnInit {
   private sub: any;
   public manPosition: number[];
   public currentMoves: number = 0;
+  public doorsPosition: number[][];
+  public keysPosition: number[][];
   /*public targetMoves: number;
 
-  public boxesPosition: number[][];
-  public targetsPosition: number[][];
+  
+  
   
   
   public hasWon: boolean = false;
@@ -51,6 +54,8 @@ export class LabyrintheComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router:Router, private LevelService: LevelService,private _hotkeysService: HotkeysService ) { 
     this.manPosition = [];
     this.setHotKeys();
+    this.doorsPosition = [];
+    this.keysPosition = [];
   }
 
 
@@ -63,6 +68,8 @@ export class LabyrintheComponent implements OnInit {
         this.rows = this.currentLevel.cases.length;
         this.columns = this.currentLevel.cases[0].length;
         this.manPosition = this.currentLevel.positions.man;
+        this.doorsPosition = this.currentLevel.positions.doors;
+        this.keysPosition = this.currentLevel.positions.keys;
         /*this.boxesPosition = this.currentLevel.positions.boxes;
         this.targetsPosition = this.currentLevel.positions.target;
         this.targetMoves = this.currentLevel["target-moves"];*/
@@ -86,15 +93,49 @@ export class LabyrintheComponent implements OnInit {
     return this.manPosition[0] == h && this.manPosition[1] == w;
   }
 
+  getDoors(h : number, w : number): number[] {
+    i:Number ;
+    for (var i = 0 ; i < this.doorsPosition.length; i++){
+      if(this.doorsPosition[i][0] == h && this.doorsPosition[i][1] == w && this.doorsPosition[i][3] == 0){
+        return [i , this.doorsPosition[i][2]];
+      }
+    }
+    return [];
+  }
+
+  getKeys(h : number, w : number): number {
+    i:Number ;
+    for (var i = 0 ; i < this.keysPosition.length; i++){
+      if(this.keysPosition[i][0] == h && this.keysPosition[i][1] == w && this.keysPosition[i][2] == 0 ){
+        return i;
+      }
+    }
+    return -1;
+  }
+
   moveLeft() {
-    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] % 2 != 1) {
+    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] % 2 != 1 && this.not_door(this.manPosition[0],this.manPosition[1], 4)) {
       this.manPosition[1] = this.manPosition[1] - 1;
       this.increaseMoves();
     }
   }
 
+  not_door(h : number, w : number , dir : number): boolean {
+    i:Number ;
+    for (var i = 0 ; i < this.doorsPosition.length; i++){
+      if(this.doorsPosition[i][0] == h && this.doorsPosition[i][1] == w && this.doorsPosition[i][2] == dir && this.doorsPosition[i][3] == 0 && this.keysPosition[i][2] == 0){
+        return false;
+      }
+      else if(this.doorsPosition[i][0] == h && this.doorsPosition[i][1] == w && this.doorsPosition[i][2] == dir && this.doorsPosition[i][3] == 0 && this.keysPosition[i][2] == 1){
+        this.doorsPosition[i][3] = 1;
+        return true;
+      }
+    }
+    return true;
+  }
+
   moveRight() {
-    if (this.pas_a_droite(this.manPosition[0],this.manPosition[1])) {
+    if (this.pas_a_droite(this.manPosition[0],this.manPosition[1]) && this.not_door(this.manPosition[0],this.manPosition[1], 2)) {
       this.manPosition[1] = this.manPosition[1] + 1;
       this.increaseMoves();
     }
@@ -113,14 +154,14 @@ export class LabyrintheComponent implements OnInit {
   }
 
   moveUp() {
-    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 8) {
+    if (this.currentLevel.cases[this.manPosition[0]][this.manPosition[1]] < 8 && this.not_door(this.manPosition[0],this.manPosition[1], 1)) {
       this.manPosition[0] = this.manPosition[0] - 1;
       this.increaseMoves();
     }
   }
 
   moveDown() {
-    if (this.pas_a_bas(this.manPosition[0],this.manPosition[1])) {
+    if (this.pas_a_bas(this.manPosition[0],this.manPosition[1]) && this.not_door(this.manPosition[0],this.manPosition[1], 3)) {
       this.manPosition[0] = this.manPosition[0] + 1;
       this.increaseMoves();
     }
@@ -160,8 +201,19 @@ export class LabyrintheComponent implements OnInit {
       if (combo === 'down') { this.moveDown(); }
       if (combo === 'left') { this.moveLeft(); }
       if (combo === 'right') { this.moveRight(); }
-      if (combo === 'space') { ; }
+      if (combo === 'space') { this.action(); }
       return false; // Prevent bubbling
     }));
+  }
+
+  action(){
+    this.on_and_take_keys();
+  }
+
+  on_and_take_keys() : void{
+    var n = this.getKeys(this.manPosition[0],this.manPosition[1]);
+    if(n != -1){//take key
+      this.keysPosition[n][2] = 1;
+    }
   }
 }
